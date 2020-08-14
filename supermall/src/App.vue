3e0e66e5d9639582f8caada40953f95de74e8662
gh2WHY <1441829719@qq.com>
2020-08-14 23:42:44 +0800

    首页开发基本完成

--- supermall/src/App.vue
+++ supermall/src/App.vue
@@ -15,9 +15,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
   @import 'assets/css/base.css';
   .tabbar {
+    z-index : 10;
     background-color: #fff;
   }
 </style>
--- supermall/src/views/Home/Home.vue
+++ supermall/src/views/Home/Home.vue
@@ -1,51 +1,36 @@
 <template>
-  <div id = 'home'>
-    <nav-bar class="home-nav"><slot slot="center">购物街</slot></nav-bar>
-    <home-swiper class="swiper" :banners="banners"/>
-    <home-recommend :recommends = 'recommends'></home-recommend>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" :titles =" ['流行','新款','精选']"></tab-control>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2><h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-      <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
-    <h2 class="title">首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <slot slot="center">购物街</slot>
+    </nav-bar>
+    <scroll class="content"
+            :probo-type = '3'
+            :pull-up-load = 'true'
+            @scroll = 'contentScroll'
+            ref = 'scroll'>
+      <home-swiper :banners="banners" />
+      <home-recommend :recommends="recommends"></home-recommend>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles=" ['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
 
+    <back-top @click.native = 'backTop' v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
-  import TabControl from '../../components/content/tabControl/tabControl'
+  import TabControl from "../../components/content/tabControl/tabControl";
+  import GoodsList from "../../components/content/goods/GoodList";
+  import Scroll from "../../components/common/BScroll/BScroll";
+  import BackTop from '../../components/content/backTop/backTop'
 
-  import HomeSwiper from './childComps/HomeSwiper'
-  import HomeRecommend from './childComps/HomeRecommend';
-  import FeatureView from './childComps/featureView'
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import HomeRecommend from "./childComps/HomeRecommend";
+  import FeatureView from "./childComps/featureView";
 
-  import {getHomeMultidata} from "network/home";
+  import { getHomeMultidata, getHomeGoods } from "network/home";
   export default {
     name: "Home",
     components: {
@@ -54,47 +39,109 @@
       HomeRecommend,
       FeatureView,
       TabControl,
+      GoodsList,
+      Scroll,
+      BackTop,
     },
     data() {
       return {
         banners: [],
         recommends: [],
+        goods: {
+          pop: { page: 0, list: [] },
+          new: { page: 0, list: [] },
+          sell: { page: 0, list: [] },
+        },
+        currentType: "pop",
+        isShowBackTop: false,
       };
     },
     created() {
-      getHomeMultidata().then((res) => {
-        // // this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-        // this.feature = res.data.
-        // console.log(this.recommends);
-      });
+      //获取首页轮播和推荐数据
+      this.getHomeMultidata();
+
+      //获取详情数据
+      this.getHomeGoods("pop");
+      this.getHomeGoods("new");
+      this.getHomeGoods("sell");
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list;
+      },
+    },
+    methods: {
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = "pop";
+            break;
+          case 1:
+            this.currentType = "new";
+            break;
+          case 2:
+            this.currentType = "sell";
+        }
+      },
+      backTop() {
+        console.log(this.$refs.scroll);
+        this.$refs.scroll.scrollTo(0,0,500)
+      },
+      contentScroll(position) {
+        this.isShowBackTop = -position.y > 1000;
+      },
+
+      //网络请求相关方法
+      getHomeMultidata() {
+        getHomeMultidata().then((res) => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        });
+      },
+
+      getHomeGoods(type) {
+        let page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then((res) => {
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        });
+      },
     },
   };
 </script>
 
-<style>
-.title {
-  color: orange;
-}
+<style scoped>
+  .title {
+    color: orange;
+  }
 
-#home {
-  padding-top : 44px;
-}
+  #home {
+    height: 100vh;
+    padding-top: 44px;
+  }
 
-.home-nav {
-  position: fixed;
-  top : 0;
-  left : 0;
-  right : 0;
-  z-index : 10;
-  background-color: var(--color-tint);
-  color: #fff;
-} 
+  .home-nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 
-.tab-control {
-  position: sticky;
-  top : 44px;
-}
+  .tab-control {
+    z-index: 3;
+    position: sticky;
+    top: 44px;
+  }
 
+  .content {
+    position: absolute;
+    top : 44px;
+    bottom : 49px;
+    left : 0;
+    right : 0;
+    overflow: hidden;
+  }
 </style>
